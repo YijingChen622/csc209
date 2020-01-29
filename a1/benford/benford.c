8f520ac158ae52df98c4b46d440e69333bcf35aa
Yijing Chen <yijing.chen@mail.utoronto.ca>
2020-01-29 11:45:28 -0500

    Deleted some useless comment, changed variable name j to num

--- a1/benford/benford.c
+++ a1/benford/benford.c
@@ -16,35 +16,23 @@ int main(int argc, char **argv) {
     } else {
     	int position = (int)strtol(argv[1], NULL, 10);
     	int tally[BASE] = {0};
-    	int j = 0;
+    	int num = 0;
     	if (argc == 3) {
     		FILE *file;
     		file = fopen(argv[2], "r");
-    		// int position = (int)strtol(argv[1], NULL, 10);
     		if (file == NULL) {
     			return 1;
     		} 
-    		// int tally[BASE] = {0};
-    		// int j = 0;
-    		while (fscanf(file, "%d", &j) == 1) {
-    			add_to_tally(j, position, tally);
+    		while (fscanf(file, "%d", &num) == 1) {
+    			add_to_tally(num, position, tally);
     		}
     		if (fclose(file) != 0) {
     			return 1;
     		}
-    		// for (int i = 0; i < BASE; i++) {
-    		// 	printf("%ds: %d\n", i, tally[i]);
-    		// }
     	} else {
-    		// int tally[BASE] = {0};
-    		// int position = (int)strtol(argv[1], NULL, 10);
-    		// int j = 0;
-    		while (scanf("%d", &j) == 1) {
-    			add_to_tally(j, position, tally);
+    		while (scanf("%d", &num) == 1) {
+    			add_to_tally(num, position, tally);
     		}
-    		// for (int i = 0; i < BASE; i++) {
-    		// 	printf("%ds: %d\n", i, tally[i]);
-    		// }
     	}
     	for (int i = 0; i < BASE; i++) {
     			printf("%ds: %d\n", i, tally[i]);
