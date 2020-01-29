c324c68900f4257f2132df85528017182ab2dcb3
Yijing Chen <yijing.chen@mail.utoronto.ca>
2020-01-29 11:35:59 -0500

    Modified benford.c

--- a1/benford/benford.c
+++ a1/benford/benford.c
@@ -14,41 +14,41 @@ int main(int argc, char **argv) {
         fprintf(stderr, "benford position [datafile]\n");
         return 1;
     } else {
+    	int position = (int)strtol(argv[1], NULL, 10);
+    	int tally[BASE] = {0};
+    	int j = 0;
     	if (argc == 3) {
     		FILE *file;
     		file = fopen(argv[2], "r");
-    		int position = (int)strtol(argv[1], NULL, 10);
+    		// int position = (int)strtol(argv[1], NULL, 10);
     		if (file == NULL) {
     			return 1;
     		} 
-    		int tally[BASE];
-    		for (int i = 0; i < BASE; i++) {
-    			tally[i] = 0;
-    		}
-    		int j = 0;
-    		while (fscanf(file, "%d", &j) != EOF) {
+    		// int tally[BASE] = {0};
+    		// int j = 0;
+    		while (fscanf(file, "%d", &j) == 1) {
     			add_to_tally(j, position, tally);
-    		}
-    		for (int i = 0; i < BASE; i++) {
-    			printf("%ds: %d\n", i, tally[i]);
     		}
     		if (fclose(file) != 0) {
     			return 1;
     		}
+    		// for (int i = 0; i < BASE; i++) {
+    		// 	printf("%ds: %d\n", i, tally[i]);
+    		// }
     	} else {
-    		int tally[BASE];
-    		int position = (int)strtol(argv[1], NULL, 10);
-    		for (int i = 0; i < BASE; i++) {
-    			tally[i] = 0;
-    		}
-    		int j = 0;
-    		while (scanf("%d", &j) != EOF) {
+    		// int tally[BASE] = {0};
+    		// int position = (int)strtol(argv[1], NULL, 10);
+    		// int j = 0;
+    		while (scanf("%d", &j) == 1) {
     			add_to_tally(j, position, tally);
     		}
-    		for (int i = 0; i < BASE; i++) {
+    		// for (int i = 0; i < BASE; i++) {
+    		// 	printf("%ds: %d\n", i, tally[i]);
+    		// }
+    	}
+    	for (int i = 0; i < BASE; i++) {
     			printf("%ds: %d\n", i, tally[i]);
     		}
-    	}
+    	return 0;
     }
-    return 0;
 }
